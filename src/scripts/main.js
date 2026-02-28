d6f03e7edc8276ca09ecd09e26bc74158faa98ad
Thanh Huy Tu <thtu1@uci.edu>
2026-02-27 17:31:06 -0800

    New build

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 // Dependencies
-import $ from "jquery";
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
