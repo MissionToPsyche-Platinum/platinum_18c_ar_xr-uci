ec579158ef4e07ae9b3bbc6a3f5953b9a41b1fa8
Thanh Huy Tu <thtu1@uci.edu>
2026-02-27 17:56:01 -0800

    New build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import inject from '@rollup/plugin-inject';
+
 // vite.config.js
 export default {
   // don't allow automatic port switching as we need to know the port
@@ -5,6 +7,12 @@ export default {
     strictPort: true,
     host: "0.0.0.0",
   },
+  plugins: [
+    inject({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
   base: "/platinum_18c_ar_xr-uci/dist",
   build: {
     outDir: "./dist",
